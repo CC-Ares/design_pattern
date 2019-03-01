dda2663f1049f147d1c6524d66f296ca158861ae
Ares <240629417@qq.com>
2019-03-01 22:43:05 +0800

    组合模式实现

--- src/main/java/cjc/ares/designpattern/composite/Company.java
+++ src/main/java/cjc/ares/designpattern/composite/Company.java
@@ -8,8 +8,11 @@ public abstract class Company {
     }
 
     public abstract void add(Company company);//add
+
     public abstract void remove(Company company);//remove
+
     public abstract void display(int depth);//display
+
     public abstract void lineofDuty();//line of duty
 
 }
