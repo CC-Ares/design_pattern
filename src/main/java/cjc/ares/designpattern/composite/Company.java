b470dbaabc63844e4eb9f72b0c7dc087a61e2e7d
Ares <240629417@qq.com>
2019-03-01 22:41:49 +0800

    桥接模式实现

---
+++ src/main/java/cjc/ares/designpattern/composite/Company.java
@@ -0,0 +1,15 @@
+package cjc.ares.designpattern.composite;
+
+public abstract class Company {
+    protected String name;
+
+    public Company(String name){
+        this.name = name;
+    }
+
+    public abstract void add(Company company);//add
+    public abstract void remove(Company company);//remove
+    public abstract void display(int depth);//display
+    public abstract void lineofDuty();//line of duty
+
+}
