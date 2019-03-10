b90a494e34987a973965f0d7ec7f21937e813495
Ares <240629417@qq.com>
2019-03-10 11:47:55 +0800

    桥接模式实现

--- src/main/java/cjc/ares/designpattern/interpreter/Test.java
+++ src/main/java/cjc/ares/designpattern/interpreter/Test.java
@@ -1,6 +1,19 @@
 package cjc.ares.designpattern.interpreter;
 
-public class Client{
+/**
+ * 优点
+ * 1、 可扩展性比较好，灵活。
+ *
+ * 2、 增加了新的解释表达式的方式。
+ *
+ * 3、 易于实现文法。
+ *
+ * 缺点
+ * 1、 执行效率比较低，可利用场景比较少。
+ *
+ * 2、 对于复杂的文法比较难维护
+ */
+public class Test{
     public static void main(String args[]){
         String statement = "3 * 2 * 4 / 6 % 5";
 
